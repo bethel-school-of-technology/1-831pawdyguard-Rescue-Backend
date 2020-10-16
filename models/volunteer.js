18a2575246d548f1b0d2d96e7e114f22c0df8170
Gabriele Fahrer <gabrielemf0325@gmail.com>
2020-10-16 17:28:31 +0200

    volunteer model and schema added

---
+++ models/volunteer.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// mongoose schema configuration
+const volunteerSchema = mongoose.Schema({
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  street: { type: String, required: true },
+  street2: { type: String},
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zip: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  details:{ type: String, required: true },
+  skills: { type: String, required: true }
+});
+
+// turn schema into a model 
+module.exports = mongoose.model('Volunteer', volunteerSchema);
+
