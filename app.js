d01f3bdff193fe8e0fe4c7f2600734ae7c962bac
Megan E. Johnson <mej2911@gmail.com>
2020-10-16 19:25:22 -0700

    app.js_michelle

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
+const Animal = require('./models/animal');
+const Volunteer = require('./models/volunteer');
 const app = express();
+
+//*****  connect application to mongodb /cloud  *****
+mongoose.connect('mongodb+srv://michelleb:gDc0HmztGIRPyjee@cluster0.0y9ug.mongodb.net/PGRescue?retryWrites=true&w=majority',
+  {useUnifiedTopology: true, useNewUrlParser: true})
+  .then(() => {
+    console.log('Connection to MongoDB established!');
+  })
+  .catch(( )=> {
+   console.log('Connection failed!');
+   });
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,12 +35,15 @@ app.use((req, res, next) => {
 
 app.post('/animalsPage', (req, res, next) => {
   // app.post('/pgr-frontend/src/app/animalsPage', (req, res, next) => {
-  const animal = req.body;
-  console.log(animal);
-  res.status(201).json({
-    message: 'Animal added successfully!',
+    const animal = new Animal({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    animal.save();
+    res.status(201).json({
+      message: 'Animal added successfully!',
+    });
   });
-});
 
 app.get('/animalsPage', (req, res, next) => {
   const animals = [
@@ -52,4 +69,31 @@ app.get('/animalsPage', (req, res, next) => {
   });
 });
 
+// ***** Add new volunteer *****
+app.post('/api/newVol', (req,res,next) => {
+  //old version without data model
+ //const newVolunteer = req.body;
+  // makes a new javascript object
+  const volunteer = new Volunteer({
+    fname: req.body.fname,
+    lname: req.body.lname,
+    street: req.body.street,
+    street2: req.body.street2,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    email: req.body.email,
+    phone: req.body.phone,
+    details:req.body.details,
+    skills: req.body.skills
+  });
+
+  volunteer.save();
+  //console.log(newVolunteer);
+  res.status(201).json({
+    message: 'Volunteer application received'
+  });
+});
+
+// ***** Export our express app to use it in server.js *****
 module.exports = app;
