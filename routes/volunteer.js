a1883faa9f444c49f17e48c2eae087c07459422e
Gabriele Fahrer <gabrielemf0325@gmail.com>
2020-10-27 10:51:41 +0100

    moved vol routes to routes folder-again and changed app.js

---
+++ routes/volunteer.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const Volunteer = require('../models/volunteer');
+
+const router = express.Router();
+
+
+// ***** Add new volunteer *****
+router.post('', (req, res, next) => {
+  const volunteer = new Volunteer({
+    fname: req.body.fname,
+    lname: req.body.lname,
+    street: req.body.street,
+    street2: req.body.street2,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    email: req.body.email,
+    phone: req.body.phone,
+    details: req.body.details,
+    ownsAnimal: req.body.ownsAnimal,
+    skills: req.body.skills
+  });
+
+  volunteer.save().then(newVolunteer => {
+    console.log(newVolunteer);
+    res.status(201).json({
+      message: 'Volunteer application received'
+    }); 
+  });
+});
+
+module.exports = router;
